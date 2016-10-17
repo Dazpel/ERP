0c3253f2198cecd73f7a365c626e5aa3bbf01ee3
judiel23 <judielsm@gmail.com>
2016-10-17 12:24:29 -0400

    modal y tabla ventas

--- app/Http/Controllers/VentasController.php
+++ app/Http/Controllers/VentasController.php
@@ -13,6 +13,55 @@ class VentasController extends Controller
     }
 
     public function postAdd(Request $request){
-    	return response()->json($request);
+
+    	$productos=array();
+    	$ingresos=array();
+    	$gastos=array();
+    	$deposito=array();
+    	$p=3;
+    	for ($i=1; $i <= intval($request->param1[2]['value']) ; $i++) { 
+    		
+
+    		array_push($productos, [
+    			"producto"=>$request->param1[$p]['value'],
+    			"cantidad"=>$request->param1[$p+1]['value'],
+			"precio_base"=>$request->param1[$p+2]['value']
+    		]);
+    		
+		
+
+		$p=$p+3;
+    	}
+    	array_push($ingresos, [
+    		"reporteZ"=>$request->param1[$p]['value'],
+    		"efectivo"=>$request->param1[$p+1]['value'],
+    		"punto"=>$request->param1[$p+2]['value']
+    		]);
+    	$p=$p+3;
+    	
+    	$v=$request->param1[$p]['value'];
+    	for ($i=1; $i <= $v ; $i++) { 
+    		
+    		array_push($gastos, [
+    			"Descripcion"=>$request->param1[$p+1]['value'],
+    			"monto" =>$request->param1[$p+2]['value']
+    			]);
+    		$p=$p+3;
+    	}
+    	array_push($deposito, [
+    		"nro"=> $request->param1[$p]['value'],
+    		"banco"=>$request->param1[$p+1]['value'],
+    		"monto"=>$request->param1[$p+2]['value']
+
+    		]);
+
+    	$V=new V();
+    	$V->ingresos=json_encode($ingresos);
+	$V->venta= json_encode($productos);
+	$V->gastos=json_encode($gastos);
+	$V->deposito=json_encode($deposito);
+	$V->save();
+
+    	return response()->json($V);
     }
 }
